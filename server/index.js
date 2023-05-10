ddc2bffd51680ca03c422846e3a16fc9e253832a
Ethan Anderson <et714492@dal.ca>
2023-05-10 02:14:39 -0300

    ^

--- server/index.js
+++ server/index.js
@@ -8,9 +8,10 @@ const cors = require("cors");
 app.use(cors());
 const server = http.createServer(app);
 
-app.get('*', (req, res) => {
+app.use(express.static(path.join(__dirname, '/../client/public/')));
+app.get('*', (req, res) => { 
     res.sendFile(path.join(__dirname + '/../client/public/index.html'));
-  });
+});
 
 // dreamgame variables
 let redisResult = "default redisResult value";
