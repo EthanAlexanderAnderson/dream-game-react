8a771aa93390bb2daa3558c025d1b0bffb0e3efe
Ethan Anderson <et714492@dal.ca>
2023-05-22 13:01:19 -0300

    fixed stat track bug

--- server/index.js
+++ server/index.js
@@ -184,11 +184,11 @@ io.on("connection", (socket) => {
             stats = stats.map(subArr => subArr.map((el, i) => i === 3 && subArr[0] === name ? parseInt(stats[statindex][3]) + 1 : el));
         }
         io.emit("update_stats", stats);
-        if (stats[statindex][0] === name) {
-            stats[statindex].shift();
+        let temp = stats[statindex].slice(0, -1);
+        if (temp[0] === name) {
+            temp.shift();
         }
-        console.log(stats[statindex].join(","));
-        client.set(("%"+name),stats[statindex].join(","));
+        client.set(("%"+name),temp.join(","));
 
         // BONUSES 
         // underdog bonus 
