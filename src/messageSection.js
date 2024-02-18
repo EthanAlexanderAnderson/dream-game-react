95f136f2bba626c2ac089870785821492223d639
Ethan Anderson <et714492@dal.ca>
2024-02-18 02:46:28 -0400

    fix react warning

--- src/messageSection.js
+++ src/messageSection.js
@@ -26,7 +26,7 @@ function MessageSection(props) {
     if (props.name !== "") {
         return (
             <div id="messageSection">
-                <div class="roundCount">Round: {props.roundNumber}</div>
+                <div className="roundCount">Round: {props.roundNumber}</div>
                 <div id="messageBox" className="message">
                     <ul>
                     {messages.map((item, index) => (
