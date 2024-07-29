2411b69a3d861d0105d891a9bf06215acff55da5
Ethan Anderson <et714492@dal.ca>
2024-07-29 00:01:23 -0300

    we have a lot of unsorted dreams

--- server/index.js
+++ server/index.js
@@ -551,7 +551,8 @@ async function updateRandomDream(type, socket){
         while ( buffer.includes(rng) || (difficulty[rng] < (averageRank - i/10) || difficulty[rng] > (averageRank + i/10)) ) 
         {
             // special case: if dream is within the last 20 (increasing) most recently added, add it if difficulty is between 4 and 6 (implies unsorted)
-            if (rng > count - (20 + i) && rng < count && difficulty[rng] > 4 && difficulty[rng] < 6){
+            // upped this to 350 temporarily because we have a lot of unsorted dreams
+            if (rng > count - (350 + i) && rng < count && difficulty[rng] > 4 && difficulty[rng] < 6){
                 break;
             }
 
